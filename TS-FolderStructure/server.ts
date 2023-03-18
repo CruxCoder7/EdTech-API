71063df01c9d50b5ac3df56e0a2f00d10488e394
R Akash <akashrangesh07@gmail.com>
2023-03-18 15:15:37 +0530

    added log stmts

--- TS-FolderStructure/server.ts
+++ TS-FolderStructure/server.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import Database from './loaders/v1/database';
 import Env from './loaders/v1/env';
+import Logger from './universe/v1/logger';
 import FrameworkLoader from './loaders/v1/framework';
 
 const server = async (): Promise<express.Application> => {
     const app = express();
     // Loaders
     Env.Loader()
+    Logger.Loader()
     await Database.Loader()
 
     FrameworkLoader(app)
